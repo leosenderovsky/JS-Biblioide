3d69c23455188ef262239ffc5d2531acae9af18f
leosenderovsky <l.a.senderovsky@gmail.com>
2021-07-18 10:50:09 -0300

    modificado js para entrega desafio 4

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-function solicitarNombre () {
-    let nombreIngresado = prompt("Ingrese su nombre");
-    alert(nombreIngresado + ", gracias por visitar nuestra librería");
-}
-solicitarNombre ();
+// Desafío 4 - Entrega 18/7
+
+let nombreIngresado = prompt("Ingrese su nombre");
+alert(nombreIngresado + ", gracias por visitar nuestra librería");
+
+let cantidadEnsayos = 0;
+let cantidadManuales = 0;
+let cantidadGuiones = 0;
+let cantidadFicciones = 0;
 
 function ingresarCantidadLibros () {
-    let cantidadEnsayos = parseInt(prompt(nombreIngresado + ", ingrese cuántos ensayos desea comprar"));
-    console.log (nombreIngresado + "desea comprar" + cantidadEnsayos + "ensayos");
+    cantidadEnsayos = parseInt(prompt(nombreIngresado + ", ingrese cuántos ensayos desea comprar"));
+    console.log (nombreIngresado + " desea comprar " + cantidadEnsayos + " ensayos");
 
-    let cantidadManuales = parseInt(prompt(nombreIngresado + ", ingrese cuántos manuales desea comprar"));
-    console.log (nombreIngresado + "desea comprar" + cantidadManuales + "manuales");
+    cantidadManuales = parseInt(prompt(nombreIngresado + ", ingrese cuántos manuales desea comprar"));
+    console.log (nombreIngresado + " desea comprar " + cantidadManuales + " manuales");
 
-    let cantidadGuiones = parseInt(prompt(nombreIngresado + ", ingrese cuántos libros de guiones desea comprar"));
-    console.log (nombreIngresado + "desea comprar" + cantidadGuiones + "libros de guiones");
+    cantidadGuiones = parseInt(prompt(nombreIngresado + ", ingrese cuántos libros de guiones desea comprar"));
+    console.log (nombreIngresado + " desea comprar " + cantidadGuiones + " libros de guiones");
 
-    let cantidadFicciones = parseInt(prompt(nombreIngresado + ", ingrese cuántos libros de ficción desea comprar"));
-    console.log (nombreIngresado + "desea comprar" + cantidadFicciones + "libros de ficción");
+    cantidadFicciones = parseInt(prompt(nombreIngresado + ", ingrese cuántos libros de ficción desea comprar"));
+    console.log (nombreIngresado + " desea comprar " + cantidadFicciones + " libros de ficción");
 
 }
 ingresarCantidadLibros ();
@@ -25,4 +29,5 @@ function sumar(cantidadEnsayos, cantidadManuales, cantidadGuiones, cantidadFicci
 }
 let resultado = sumar(cantidadEnsayos, cantidadManuales, cantidadGuiones, cantidadFicciones);
 sumar ();
-alert(nombreIngresado + ", usted desea comprar" + resultado + "libros");
+
+alert(nombreIngresado + ", usted desea comprar " + resultado + " libros");
