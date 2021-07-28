6a56287ce1f3e6e9a9e497afea7bc3aa956cad2f
leosenderovsky <l.a.senderovsky@gmail.com>
2021-07-28 18:57:32 -0300

    Etapa 1 de armado prueba carrito sobre tutorial en archivo json y app.js

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+})
+
+items.addEventListener('click', e => {
+    addCarrito(e);
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json');
+        const data = await res.json();
+        // console.log(data);
+        pintarCards(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const pintarCards = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.titulo;
+        templateCard.querySelector('p').textContent = producto.precio;
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector('.btn-primary').dataset.id = producto.id;
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    items.appendChild(fragment);
+}
+
+const addCarrito = e => {
+    //console.log(e.target);
+    //console.log(e.target.classList.contains('btn-primary'));
+    if(e.target.classList.contains('btn-primary')){
+        console.log(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    console.log(objeto);
+    const producto = {
+        id: objeto.querySelector('.btn-primary').dataset.id,
+        titulo: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1,
+    }
+    console.log(producto);
+}
