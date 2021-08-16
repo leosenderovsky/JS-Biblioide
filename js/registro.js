c861aa16b41d2aa2deb25aabd5d6784c39c6ca5d
Leo Aquiba Senderovsky <l.a.senderovsky@gmail.com>
2021-08-16 11:29:13 -0300

    modificacion registro

--- js/registro.js
+++ js/registro.js
@@ -1,7 +1,52 @@
 import { User } from "./user.js";
 import { UI } from "./UI.js";
 
-document.getElementById("register")
+const form = document.getElementById('register')
+form.innerHTML = `
+          <div class="form-outline mb-4">
+            <input type="name" id="name" class="form-control" required />
+            <label for="name" class="form-label">Nombre y apellido</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="username" id="username" class="form-control" required />
+            <label for="username" class="form-label">Nombre de usuario</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="email" id="email" class="form-control" required />
+            <label for="email" class="form-label">Email</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="password" id="password" class="form-control" required />
+            <label for="password" class="form-label">Contraseña</label>
+          </div>
+          <div class="form-outline mb-4">
+            <label class="form-label">Teléfono</label>
+            <input id="phone" type="tel" name="phone" />            
+            <!--<input type="submit" class="btn" value="Verificar" />-->
+          </div>
+          <div class="form-outline mb-4">
+            <select id="country-dropdown" name="country" class="form-control">
+            </select>
+            <label class="form-label">País</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="city" id="city" class="form-control" />
+            <label class="form-label">Ciudad</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="address" id="address" class="form-control" />
+            <label class="form-label">Dirección</label>
+          </div>
+          <div class="form-outline mb-4">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label class="form-check-label" for="form1Example3">Recordarme
+              </label>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Ingresar</button>`
+
+//document.getElementById("register")
 document.addEventListener("submit", function (e) {
 
   e.preventDefault();
@@ -22,6 +67,7 @@ document.addEventListener("submit", function (e) {
   ui.showMessage("Por favor, complete todos los campos", "danger");
   }
 
+  if (name != "" || username != "" || email != "" || password != "" || phone != "" || country != "" || city != "" || address != "")
   ui.addUser(user);
   ui.showMessage(username + ", gracias por ingresar en la plataforma", "success");
   ui.resetForm();
