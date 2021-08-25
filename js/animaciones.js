46f8eb6a25b3a44bcb9e56245284e0cdcfee9236
leosenderovsky <l.a.senderovsky@gmail.com>
2021-08-24 21:08:29 -0300

    actualizado animaciones.js con animacion de h1

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,9 +1,17 @@
+// INTENTANDO QUE SE PRODUZCA UNA ANIMACIÓN SOBRE EL BOTÓN DE LAS CARDS
+
 $(".btn").mouseover(function() {
     $(".btn").animate({
         color: '#357376',
         backgroundColor: '#E5DFDF',
     }, "slow");        
 });
+
+// ANIMACIÓN DE ZOOM IN Y OUT AL H1 DEL SITIO
+
+$(".title").mouseover(() =>
+$(".title").animate({'zoom': 0.9}, 400).delay(100).animate({'zoom': 1}, 400));
+
 
 // ANIMACIÓN PARA QUE AL HACER CLICK EN EL TÍTULO DE LA CATEGORÍA EN LA HOME APAREZCA CON FADE BOTÓN DE VER MÁS 
 
